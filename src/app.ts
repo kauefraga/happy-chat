cf5e5ca9bd5e20efa21d5f301e2f8bbfa9063190
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-11-13 18:12:09 -0300

    :recycle: chore: serve static files and separate websocket definitions

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,15 @@
 import http from 'node:http';
 import express from 'express';
 import { router } from './routes';
-import { Server } from 'socket.io';
+import { createWebsocketServer } from 'websocket';
 
 export function createServer() {
   const app = express();
   const server = http.createServer(app);
-  const io = new Server(server, {
-    path: '/ws'
-  });
 
+  createWebsocketServer(server);
+  app.use(express.static(__dirname + '/views'));
   app.use(router);
-
-  io.on('connection', (socket) => {
-    console.log(' WEBSOCKET  New connection!')
-
-    socket.on('disconnect', () => {
-      console.log(` SOCKET (${socket.id})  Disconnected!`);
-    });
-
-    socket.on('chat message', (message: string) => {
-      console.log(` SOCKET (${socket.id})  Message: ${message}`);
-    });
-
-    socket.on('chat message', (message: string) => {
-      const denyList = [ 'VAI SE FUDER', 'VAI TOMAR NO CU', 'STFU' ];
-
-      for (let i = 0; i < denyList.length; i++) {
-        if (message === denyList[i]) {
-          console.log(` SOCKET (${socket.id})  Says '${message}'`)
-          message = '****';
-        }
-      }
-
-      io.emit('chat message', socket.id, message);
-    });
-  });
 
   return server;
 }
